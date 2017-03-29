8ff2d5dfae99d320bad524be0eb5d1c4e5de432a
Ben <benschenkenberger@gmail.com>
2017-03-29 07:22:11 -0700

    Update to do list for 'one-to-many' database relationships lesson

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -16,19 +15,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/categories/:id/tasks/new", (request, response) -> {
+    get("categories/:id/tasks/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.params(":id")));
       model.put("category", category);
       model.put("template", "templates/category-tasks-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-    // get("tasks/new", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/task-form.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
 
     get("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
@@ -37,23 +30,19 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/tasks", (request,response) -> {
+    post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-
       Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
-
       String description = request.queryParams("description");
       Task newTask = new Task(description);
-
       category.addTask(newTask);
-
       model.put("category", category);
-      model.put("template", "templates/category-tasks-success.vtl");
+      model.put("template", "templates/category-task-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/tasks/:id", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
+      Map<String, Object> model = new HashMap<String, Object>();
       Task task = Task.find(Integer.parseInt(request.params(":id")));
       model.put("task", task);
       model.put("template", "templates/task.vtl");
@@ -88,38 +77,6 @@ public class App {
       model.put("template", "templates/category.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
   }
 }
-
-
-// public class App {
-//   public static void main(String[] args) {
-//     staticFileLocation("/public");
-//     String layout = "templates/layout.vtl";
-//
-//     get("/", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       model.put("tasks", request.session().attribute("tasks"));
-//       model.put("template", "templates/index.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     post("/tasks", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//
-//       ArrayList<Task> tasks = request.session().attribute("tasks");
-//       if (tasks == null) {
-//         tasks = new ArrayList<Task>();
-//         request.session().attribute("tasks", tasks);
-//       }
-//
-//       String description = request.queryParams("description");
-//       Task newTask = new Task(description);
-//       tasks.add(newTask);
-//       // request.session().attribute("task", newTask);
-//
-//       model.put("template", "templates/success.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//   }
-// }
